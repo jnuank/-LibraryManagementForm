803f19262ec57af5a0a0351e0c9b888d95f098dc
Jun.Nakajima <ikuta1919@gmail.com>
2018-04-14 09:43:45 +0900

    【add】ExceptionHandler.cs追加（例外ハンドラ）

---
+++ LibraryManagement/Common/exception/ExceptionHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Common.exception
+{
+    /// <summary>
+    /// 蔵書管理システムの例外ハンドラクラス
+    /// </summary>
+    public static class ExceptionHandler
+    {
+        public static void ApplicationThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+            // 入力系の例外だった場合には、業務エラーメッセージを表示して、例外箇所にフォーカスを設定する
+            if(e.Exception is InputException)
+            {
+                var ex = e.Exception as InputException;
+
+                MessageBox.Show(ex.Message);
+                ex.ERROR_TEXTBOX.Clear();
+                ex.ERROR_TEXTBOX.Focus();
+            }
+            else
+            {
+                MessageBox.Show(e.Exception.GetType().ToString());
+                Environment.FailFast("FailFastのテストです");
+            }
+        }
+    }
+}
