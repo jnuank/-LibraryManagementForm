83a954396b6351ef66773c2ecd8f7d31bd03702d
Jun.Nakajima <ikuta1919@gmail.com>
2018-04-14 15:58:51 +0900

    【add】BookManagementLogic.cs

---
+++ LibraryManagement/BCMT01/logic/BookManagementLogic.cs
@@ -0,0 +1,100 @@
+﻿using Common.control;
+using Common.define;
+using Common.ErrorCheck;
+using System.Windows.Forms;
+
+namespace BCMT01.logic
+{
+    public class BookManagementLogic
+    {
+        /// <summary>
+        /// データグリッドビューに表示するカラム
+        /// </summary>
+        private enum COLUMNS
+        {
+            ID,
+            NAME,
+            DIVISION_ID1,
+            DIVISION_ID2,
+            DIVISION_ID3,
+            ARRIVAL_USER_ID,
+            ARRIVAL_DATE,
+        }
+
+        /// <summary>
+        /// DataGridView初期化
+        /// </summary>
+        /// <param name="dataGridView"></param>
+        public void InitGridView(DataGridViewCustom dataGridView)
+        {
+            // データソースの設定をする
+            dataGridView.InitDataSource();
+            // コントロールの初期化
+            dataGridView.InitControl();
+
+            // 書籍IDセル
+            dataGridView.Columns[(int)COLUMNS.ID].HeaderText   = GlobalDefine.BOOK_ID;
+            dataGridView.Columns[(int)COLUMNS.ID].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+
+            // タイトル
+            dataGridView.Columns[(int)COLUMNS.NAME].HeaderText = GlobalDefine.BOOK_NAME;
+        }
+
+        /// <summary>
+        /// 検索結果一覧をクリアする
+        /// </summary>
+        /// <param name="dataGridView"></param>
+        public void ClearResult(DataGridViewCustom dataGridView)
+        {
+            dataGridView.Table.Clear();
+            this.InitGridView(dataGridView);
+        }
+
+        /// <summary>
+        /// 検索条件をクリアする
+        /// </summary>
+        /// <param name="control"></param>
+        public void ClearInputBox(Control parentControl)
+        {
+            foreach (Control control in parentControl.Controls)
+            {
+                if (control.HasChildren)
+                    ClearInputBox(control);
+
+                if(control is TextBox)
+                {
+                    var textBox = control as TextBox;
+                    textBox.Clear();
+                }
+                else if (control is CategoryDropDownList)
+                {
+                    var cmbBox = control as CategoryDropDownList;
+                    cmbBox.SelectedIndex = 0;
+                }
+            }
+        }
+
+        /// <summary>
+        /// エラーチェック
+        /// </summary>
+        /// <param name="txtId"></param>
+        /// <param name="txtTitle"></param>
+        public void ErrorChecks(TextBox txtId, TextBox txtTitle)
+        {
+            InputCheck.IsSingleQuotation(txtId);
+            InputCheck.IsSingleQuotation(txtTitle);
+        }
+
+        /// <summary>
+        /// コンボボックスの初期化
+        /// </summary>
+        /// <param name="cmbCategorys"></param>
+        public void InitDialog(params CategoryDropDownList[] cmbCategorys)
+        {
+            foreach (var item in cmbCategorys)
+            {
+                item.InitControl();
+            }
+        }
+    }
+}
---
+++ LibraryManagement/UnitTest/Common/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Common.ErrorCheck;
+using Common.exception;
+using System.Windows.Forms;
+
+namespace UnitTest.Common
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        TextBox txtBox = new TextBox();
+
+
+        [TestMethod]
+        [ExpectedException(typeof(InputException))]
+        public void TestMethod1()
+        {
+            txtBox.Text = "'";
+            InputCheck.IsSingleQuotation(txtBox);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            txtBox.Text = "'";
+            Assert.IsTrue(txtBox.CheckSingleQuotation());
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            txtBox.Text = " ";
+            Assert.IsFalse(txtBox.CheckSingleQuotation());
+        }
+
+    }
+}
